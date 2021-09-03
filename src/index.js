484530efe46b31111a7e7b8841e83d212dca8dcd
Tim Marchant <fromtimwithcode@gmail.com>
2021-09-03 16:52:36 -0500

    Dark Mode

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import axios from "axios";
-
-import Charts from "./components/Charts";
-import Navbar from "./components/Navbar";
-
-
-import "./styles.scss";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+import Charts from './components/Charts';
+import Navbar from './components/Navbar';
+import useDarkMode from './hooks/useDarkMode';
+import './styles.scss';
 
 const App = () => {
   const [coinData, setCoinData] = useState([]);
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode(false);
 
   useEffect(() => {
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
-      )
-      .then(res => setCoinData(res.data))
-      .catch(err => console.log(err));
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true')
+      .then(res => {
+        setCoinData(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      });
   }, []);
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
